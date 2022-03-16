a98798674279389651fb0b1a5febd8833888bc18
chaulagainn <niraj.chaulagain@outlook.com>
2022-03-16 11:28:33 -0700

    lunch 3/16

---
+++ GroupDelete.ps1
@@ -0,0 +1,23 @@
+﻿$user = Read-Host "Enter Username of the user"
+
+Get-ADUser $user
+
+
+$usergrps = Get-ADPrincipalGroupMembership $user | select name
+##Wite-Host ($usergrps)
+
+
+
+foreach ($groups in $usergrps)
+{
+    Write-Host $groups.name
+    if ($groups.name -eq "Domain Users"){Write-Host "Not Deleted from Domain Users group"}
+    else{
+            Remove-ADPrincipalGroupMembership $user -MemberOf $groups.name;
+            Write-Host "Deleted"
+        
+    }
+
+
+
+}
---
+++ processStop.ps1
@@ -0,0 +1,13 @@
+﻿########Stop and start process on remote host
+
+####step1 Get processes on a remote host
+
+Get-Process notepad | get-member
+
+
+##############computer last logon
+
+$computername = Read-Host ("Enter the computer name")
+Get-ADComputer $computername -Properties * | select lastlogondate | ft
+
+
